7fcc5fec695ea7670ac9cf4ce3d1a89680109d49
Yash Sarang <yash.sarang9@gmail.com>
2024-03-06 00:26:18 +0530

    Created NavBar and Hero Section

---
+++ instructions.md
@@ -0,0 +1,9 @@
+npm create vite@latest
+Enter Project Name
+Framework - React
+Variant - Javascript
+
+npm i 
+"npm run dev" to start working 
+
+Start with learning to make a NavBar
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import Logo_MedRes from "../assestsFInal/Logo_MedRes.jpg";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img className="navbar-logo-image" src={Logo_MedRes} alt="Logo" />
+      </div>
+      <div className={`navbar-menu ${isMenuOpen ? "open" : ""}`}>
+        <ul className="navbar-items">
+          <li className="navbar-item">Home</li>
+          <li className="navbar-item">About</li>
+          <li className="navbar-item">Blogs</li>
+          <li className="navbar-item">RoadMaps</li>
+          <li className="navbar-item">Contact</li>
+        </ul>
+      </div>
+      <div className="navbar-toggle" onClick={toggleMenu}>
+        <div className={`navbar-toggle-icon ${isMenuOpen ? "open" : ""}`}></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
